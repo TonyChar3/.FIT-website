1bc8efe41b6f630190a592c9ac135e1d947aed60
Anthony <charetteanthony@outlook.com>
2023-12-12 08:33:26 -0500

    FIX: white blank screen on mobile

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    host: '127.0.0.1'
-  },
-  base:"/"
+  plugins: [react()]
 });
 
 // Home: '10.0.0.78'
 // School: '142.169.16.15'
+//  server: {
+//   port: 3000,
+//   host: '127.0.0.1'
+// },
+// base:"/"
