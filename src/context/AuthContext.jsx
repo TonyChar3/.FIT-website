d4b5bf18ad993611223d8bd9727f70f9f2114e82
Anthony <charetteanthony@outlook.com>
2023-12-12 20:47:30 -0500

    FIX: issues cookies being set up the first load

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -169,11 +169,13 @@ export const AuthContextProvider = ({ children }) => {
         // get the consent of cookies from the storage
         const cookie_state_consent = Cookies.get('CookieConsent');
         setCookieConsent(cookie_state_consent);
-    },[cookie_consent,user]);
+    },[]);
 
     useEffect(() => {
         if(cookie_consent === false){
             ClearCookies();
+        } else if(cookie_consent === true){
+            GetFitUserInfo();
         }
     },[cookie_consent])
 
