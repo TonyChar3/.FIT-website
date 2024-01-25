e0e728036a69a3035292b7de4b1c7a15fc3b39f6
Tony Char3 <tonyc6731@gmail.com>
2024-01-24 23:12:38 -0500

    FIX: error on the profile page for the wishlist

--- src/components/Navbar/profile/User/Wishlist/wishlist.jsx
+++ src/components/Navbar/profile/User/Wishlist/wishlist.jsx
@@ -25,63 +25,69 @@ const WishList = () => {
             ></i>
           </div>
         </div>
-        <div
-          className={`
-                grid grid-cols-1 gap-4 p-2 w-full
-                lg:grid-cols-${
-                  wishlist_array.length && wishlist_array.length > 2
-                    ? "2"
-                    : wishlist_array.length
-                    ? wishlist_array.length
-                    : "1"
-                } 
-                ${
-                  Array.isArray(wishlist_array) && wishlist_array.length > 2
-                    ? "lg:grid-rows-2"
-                    : ""
-                } 
-                ${
-                  chevron
-                    ? "static translate-x-0 z-0 duration-300"
-                    : "absolute -translate-x-[200px] -z-20 h-0"
-                } lg:my-4
-                `}
-        >
-          {Array.isArray(wishlist_array) && wishlist_array.length ? (
-            wishlist_array.map((wish, i) => (
-              <div
-                key={i}
-                className={`flex flex-row justify-around p-2 my-2 w-full ${
-                  chevron
-                    ? "static translate-x-0 z-0 duration-700"
-                    : "relative -translate-x-[200px] w-0 -z-20"
-                }`}
-              >
-                <div className="w-1/3 flex flex-row justify-center items-center">
-                  <img
-                    src={wish.prodct_img}
-                    alt="product image"
-                    width="100"
-                    height="100"
-                    className="w-20 h-20 border border-black object-cover object-center rounded-xl lg:w-[100px] lg:h-[120px]"
-                  />
+        {wishlist_array.length ? (
+          <div
+            className={`
+                      grid grid-cols-1 gap-4 p-2 w-full
+                      lg:grid-cols-${
+                        Array.isArray(wishlist_array) &&
+                        wishlist_array.length > 2
+                          ? "2"
+                          : wishlist_array.length
+                      } 
+                      ${
+                        Array.isArray(wishlist_array) &&
+                        wishlist_array.length > 2
+                          ? "lg:grid-rows-2"
+                          : ""
+                      } 
+                      ${
+                        chevron
+                          ? "static translate-x-0 z-0 duration-300"
+                          : "absolute -translate-x-[200px] -z-20 h-0"
+                      } lg:my-4
+                      `}
+          >
+            {Array.isArray(wishlist_array) && wishlist_array.length ? (
+              wishlist_array.map((wish, i) => (
+                <div
+                  key={i}
+                  className={`flex flex-row justify-around p-2 my-2 w-full ${
+                    chevron
+                      ? "static translate-x-0 z-0 duration-700"
+                      : "relative -translate-x-[200px] w-0 -z-20"
+                  }`}
+                >
+                  <div className="w-1/3 flex flex-row justify-center items-center">
+                    <img
+                      src={wish.prodct_img}
+                      alt="product image"
+                      width="100"
+                      height="100"
+                      className="w-20 h-20 border border-black object-cover object-center rounded-xl lg:w-[100px] lg:h-[120px]"
+                    />
+                  </div>
+                  <div className="flex flex-row justify-center items-center w-1/3 justify-center">
+                    <h3 className="text-lg lg:text-2xl">{wish.prodct_name}</h3>
+                  </div>
+                  <div className="flex flex-row items-center w-1/3 justify-center">
+                    <span className="text-lg lg:text-xl">
+                      {wish.prodct_price}$
+                    </span>
+                  </div>
                 </div>
-                <div className="flex flex-row justify-center items-center w-1/3 justify-center">
-                  <h3 className="text-lg lg:text-2xl">{wish.prodct_name}</h3>
-                </div>
-                <div className="flex flex-row items-center w-1/3 justify-center">
-                  <span className="text-lg lg:text-xl">
-                    {wish.prodct_price}$
-                  </span>
-                </div>
+              ))
+            ) : (
+              <div className="text-xl my-2 text-center">
+                Nothing in the wishlist :(
               </div>
-            ))
-          ) : (
-            <div className="text-xl my-2 text-center">
-              Nothing in the wishlist :(
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+        ) : (
+          <div>
+            <h2>Wait a moment...</h2>
+          </div>
+        )}
       </div>
     </>
   );
